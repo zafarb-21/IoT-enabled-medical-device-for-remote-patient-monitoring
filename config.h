00fb9e73c3cba0bcb9791cffe34d4f4092ed0ea0
Victor Basvi <766cwqdyq2@privaterelay.appleid.com>
2025-11-19 22:25:18 -0500

    Update and rename Software Configuration to config.h

---
+++ config.h
@@ -0,0 +1,19 @@
+// config.h - Configuration and secrets
+#define WIFI_SSID "Your_WiFi_SSID"
+#define WIFI_PASSWORD "Your_WiFi_Password"
+#define MQTT_SERVER "your.mqtt.broker.ip"
+#define MQTT_PORT 1883
+#define MQTT_USER "mqtt_user"
+#define MQTT_PASSWORD "mqtt_password"
+#define MQTT_TOPIC "patient/vitals"
+
+// Device configuration
+#define PATIENT_ID "patient_001"
+#define SEND_INTERVAL 30000  // 30 seconds
+#define DEEP_SLEEP_TIME 300  // 5 minutes sleep when on battery
+
+// Alert thresholds
+#define MAX_HEART_RATE 120
+#define MIN_HEART_RATE 40
+#define MAX_TEMPERATURE 38.5  // °C
+#define MIN_SPO2 90           // %
