d1857bfffee1b08e17fa7e0bd3338949270962d4
Victor Basvi <766cwqdyq2@privaterelay.appleid.com>
2025-11-23 15:22:42 -0500

    Update WiFi and MQTT configuration details

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 // config.h - Configuration and secrets
-#define WIFI_SSID "Your_WiFi_SSID"
-#define WIFI_PASSWORD "Your_WiFi_Password"
-#define MQTT_SERVER "your.mqtt.broker.ip"
+#define WIFI_SSID "Victor's iPhone"
+#define WIFI_PASSWORD "Password"
+#define MQTT_SERVER "a5ef99d0bdcd45feb91d4bd3881df6de.s1.eu.hivemq.cloud"
 #define MQTT_PORT 1883
-#define MQTT_USER "mqtt_user"
-#define MQTT_PASSWORD "mqtt_password"
+#define MQTT_USER "admin"
+#define MQTT_PASSWORD "Trishbasvi@2021"
 #define MQTT_TOPIC "patient/vitals"
 
 // Device configuration
